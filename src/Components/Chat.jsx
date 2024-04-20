f41d31e2559e31d99a17a70e50379bc1887fe260
Kevin Pérez Escamilla <perez.kevin.1cv.2017@gmail.com>
2024-04-19 20:24:23 -0600

    chat

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import './Chat.css'
 import axios from "axios";
 import ReactMarkdown from "react-markdown";
 
@@ -35,10 +34,10 @@ function App() {
 
     return (
         <>
-            <div className="bg-cyan-800 w-4/5 h-5/6 text-white Chat-container">
+            <div className="bg-cyan-800 w-4/5 h-5/6 text-white rounded-2xl">
                 <h1 className="text-3xl text-center">Chat AI + With Gemini</h1>
-                <form onSubmit={generateAnswer} className="Chat-formulary">
-                    <div className="Gemini-ia-container">
+                <form onSubmit={generateAnswer} className=" w-full h-5/6 p-8">
+                    <div className=" h-full w-full overflow-y-auto bg-slate-200">
                         <ReactMarkdown className="">{answer}</ReactMarkdown>
                     </div>
                     <div className="bg-cyan-800 text-white submit-controls">
