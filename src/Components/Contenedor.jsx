0476900f01abd7883d35516ed7414a43006c3e62
Kevin Pérez Escamilla <perez.kevin.1cv.2017@gmail.com>
2024-04-19 19:00:04 -0600

    avance

--- src/Components/Contenedor.jsx
+++ src/Components/Contenedor.jsx
@@ -3,7 +3,7 @@
 function Contenedor({children, title=""}) {
     return <div className="w-5/6 h-2/5 bg-cyan-800 rounded-2xl p-5">
         <h3 className=" font-extrabold text-slate-200">{title}</h3>
-        {/* {children} */}
+        {children}
     </div>
 }
 
